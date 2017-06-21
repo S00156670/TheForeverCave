accfee78b711fa0c1a2866b91d70e1adb6b81a8c
bryan foley <s00156670@mail.itsligo.ie>
2017-06-21 03:40:23 +0100

    weapon equip with related status changes

--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -3,20 +3,62 @@ using System.Collections;
 
 public class PlayerWeaponController : MonoBehaviour {
 
+    public GameObject playerHand;
+    public GameObject EquippedWeapon { get; set; }
+
+    IWeapon equippfedWeaponData;
+
+    CharachterStats charachterStats;
+
+    // Use this for initialization
+    void Start()
+    {
+        charachterStats = GetComponent<CharachterStats>();
+    }
+
     //equip 
+    public void EquipWeapon(Item itemToEquip)
+    {
+        if (EquippedWeapon != null)
+        {
+            //   GetComponent<CharachterStats>
+            // remove buffs from previous weapon
+            charachterStats.RemoveStatBonus(EquippedWeapon.GetComponent<IWeapon>().Stats);
+            // remove previous weapon from players hand
+            Destroy(playerHand.transform.GetChild(0).gameObject);
+        }
+
+        // hand is now empty :)
+
+        // instance of weapon generated at players hand
+        EquippedWeapon = (GameObject)Instantiate(Resources.Load<GameObject>("Weapon/" + itemToEquip.ObjectSlug), playerHand.transform.position, playerHand.transform.rotation);
+
+        // extract weapon info
+        equippfedWeaponData = EquippedWeapon.GetComponent<IWeapon>();
+
+        // set stats of equipped weapon
+        // should be remembered so you know what to remove when you unequip
+        //     EquippedWeapon.GetComponent<IWeapon>().Stats = itemToEquip.Stats;
+        equippfedWeaponData.Stats = itemToEquip.Stats;
+        EquippedWeapon.transform.SetParent(playerHand.transform);
+        charachterStats.AddStatBonus(itemToEquip.Stats);
+
+        Debug.Log(equippfedWeaponData.Stats);
+
+    }
+
     // use 
+    public void PerformAttack()
+    {
+        //   EquippedWeapon.GetComponent<IWeapon>().PerformAttack();
+        equippfedWeaponData.PerformAttack();
+    }
+
     // return to inventory
     // drop
 
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
 }
