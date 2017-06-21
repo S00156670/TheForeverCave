beb1d75eb6af41a89f460f3485fc0a2a78a7d34c
bryan foley <s00156670@mail.itsligo.ie>
2017-06-21 01:36:56 +0100

    Charachter statuseffect  controll and item elaboration

--- Assets/Scripts/CharachterStats.cs
+++ Assets/Scripts/CharachterStats.cs
@@ -8,11 +8,34 @@ public class CharachterStats : MonoBehaviour {
 
     private void Start()
     {
+        // set up stats here
+        // should be done mainly fron children (player , skelaton etc.)
         stats.Add(new global::BaseStat(4, "Power", "Your Power Level"));
+        stats.Add(new global::BaseStat(10, "Vitality", "Your Vitality Level"));
+
         stats[0].AddStatBonus(new StatBonus(5));
 
         Debug.Log(stats[0].GetCalculatedStatValue());
 
     }
+
+    public void AddStatBonus(List<BaseStat> statusChange)
+    {
+        foreach (BaseStat statCheck in statusChange)
+        {
+            // check for matching catagory then when found puts a bonus to the current stat being checked
+            stats.Find(x => x.StatName == statCheck.StatName).AddStatBonus(new StatBonus(statCheck.BaseValue));
+        }
+    }
+
+    public void RemoveStatBonus(List<BaseStat> statusChange)
+    {
+        foreach (BaseStat statCheck in statusChange)
+        {
+            // check for matching catagory then when found takes a bonus from the current stat being checked
+            stats.Find(x => x.StatName == statCheck.StatName).RemoveStatBonus(new StatBonus(statCheck.BaseValue));
+        }
+    }
+
 
 }
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item  {
+
+    public List<BaseStat> Stats { get; set; }
+    public string ObjectSlug { get; set; }
+
+    public Item(List<BaseStat> stats , string objectSlug)
+    {
+        Stats = stats;
+        ObjectSlug = objectSlug;
+
+    }
+
+}
