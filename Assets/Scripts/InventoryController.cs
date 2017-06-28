f1c8bdd8e293ca9bfe5a7894f4739efffed4f094
bryan foley <s00156670@mail.itsligo.ie>
2017-06-28 03:19:58 +0100

    drinkable potion

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class InventoryController : MonoBehaviour {
 
     public PlayerWeaponController playerWeaponController;
+    public PlayerConsumableController playerConsumableController;
+
     public Item sword;
 
     public Item potion;
@@ -12,6 +14,7 @@ public class InventoryController : MonoBehaviour {
     private void Start()
     {
         playerWeaponController = GetComponent<PlayerWeaponController>();
+        playerConsumableController = GetComponent<PlayerConsumableController>();
 
         List<BaseStat> swordStats = new List<BaseStat>();
         swordStats.Add(new BaseStat(6, "Power","Your Power Level"));
@@ -19,7 +22,7 @@ public class InventoryController : MonoBehaviour {
         // temp sword/staff slug swap
         sword = new Item(swordStats, "staff");
 
-        potion = new Item(new List<BaseStat>(), "potion_test","drink this to test potipn drinking","Drink","PotionTest",false);
+        potion = new Item(new List<BaseStat>(), "potion","drink this to test potipn drinking","Drink","PotionTest",false);
 
     }
 
@@ -31,6 +34,11 @@ public class InventoryController : MonoBehaviour {
        //     sword.ObjectSlug = "sword";
 
             playerWeaponController.EquipWeapon(sword);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            playerConsumableController.ConsumeItem(potion);
         }
 
 
---
+++ Assets/Scripts/PlayerConsumableController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerConsumableController : MonoBehaviour {
+
+    CharachterStats stats;
+
+
+
+	// Use this for initialization
+	void Start () {
+        stats = GetComponent<CharachterStats>();
+	
+	}
+
+    public void ConsumeItem(Item item)
+    {
+        GameObject itemToHandle = Instantiate(Resources.Load<GameObject>("Consumables/" + item.ObjectSlug));
+
+        if (item.StatModifier)
+        {
+            itemToHandle.GetComponent<IConsumable>().Consume(stats);
+        }
+        else
+        {
+            itemToHandle.GetComponent<IConsumable>().Consume();
+        }
+
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
