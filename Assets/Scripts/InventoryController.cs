b5ad83e1524462bb285e0b565cba09a7445d7c12
bryan foley <s00156670@mail.itsligo.ie>
2017-06-28 04:18:36 +0100

    stripping out of hardcoded objects

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -7,60 +7,63 @@ public class InventoryController : MonoBehaviour {
     public PlayerWeaponController playerWeaponController;
     public PlayerConsumableController playerConsumableController;
 
-    public Item sword;
+    public static InventoryController instance { get; set; }
 
-    public Item potion;
+    //public Item sword;
+
+    //public Item potion;
 
     private void Start()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            // there can only be only one
+        }
+        else
+        {
+            instance = this;
+        }
+
         playerWeaponController = GetComponent<PlayerWeaponController>();
         playerConsumableController = GetComponent<PlayerConsumableController>();
 
-        List<BaseStat> swordStats = new List<BaseStat>();
-        swordStats.Add(new BaseStat(6, "Power","Your Power Level"));
 
-        // temp sword/staff slug swap
-        sword = new Item(swordStats, "staff");
+        //// generating test hardcoded objects
+        //List<BaseStat> swordStats = new List<BaseStat>();
+        //swordStats.Add(new BaseStat(6, "Power","Your Power Level"));
 
-        potion = new Item(new List<BaseStat>(), "potion","drink this to test potipn drinking","Drink","PotionTest",false);
+        //// temp sword/staff slug swap
+        //sword = new Item(swordStats, "staff");
 
-    }
-
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-
-       //     sword.ObjectSlug = "sword";
-
-            playerWeaponController.EquipWeapon(sword);
-        }
-
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            playerConsumableController.ConsumeItem(potion);
-        }
-
-
-
-        //if (Input.GetKeyDown(KeyCode.Keypad1))
-        //{
-
-        //    sword.ObjectSlug = "sword";
-
-        //    playerWeaponController.EquipWeapon(sword);
-        //}
-        //if (Input.GetKeyDown(KeyCode.Z))
-        //{
-
-        //    sword.ObjectSlug = "staff";
-
-        //    playerWeaponController.EquipWeapon(sword);
-        //}
-
-
-
+        //potion = new Item(new List<BaseStat>(), "potion","drink this to test potipn drinking","Drink","PotionTest",false);
 
     }
+
+    ////test code for hardcoded items
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Z))
+    //    {
+    //   //     sword.ObjectSlug = "sword";
+    //        playerWeaponController.EquipWeapon(sword);
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.P))
+    //    {
+    //        playerConsumableController.ConsumeItem(potion);
+    //    }
+
+    //    //if (Input.GetKeyDown(KeyCode.Keypad1))
+    //    //{
+    //    //    sword.ObjectSlug = "sword";
+    //    //    playerWeaponController.EquipWeapon(sword);
+    //    //}
+    //    //if (Input.GetKeyDown(KeyCode.Z))
+    //    //{
+    //    //    sword.ObjectSlug = "staff";
+
+    //    //    playerWeaponController.EquipWeapon(sword);
+    //    //}
+    //}
 
 }
