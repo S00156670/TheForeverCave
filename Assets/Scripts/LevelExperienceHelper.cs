20e51c24ce6107f655ce54c0e547672639bf7586
bryan foley <s00156670@mail.itsligo.ie>
2017-08-07 23:52:16 +0100

    adding experience

---
+++ Assets/Scripts/LevelExperienceHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExperienceHelper : MonoBehaviour {
+
+    public int Level { get; set; }
+    public int CurrentExperiene { get; set; }
+    public int RequiredExperience { get { return Level * 25; } }
+
+    // Use this for initialization
+    void Start()
+    {
+        Level = 1;
+    }
+
+    public void EnemyToExperience(IEnemy enemy)
+    {
+        GrantExperience(enemy.Experience);
+    }
+
+
+    public void GrantExperience(int amount)
+    {
+        CurrentExperiene += amount;
+
+
+    }
+
+
+}
