35a8b13a7e88ec43a20b919c209e040336d851d1
bryan foley <s00156670@mail.itsligo.ie>
2017-08-23 01:03:53 +0100

    trying to implement basic playerstat to gameplay relationship

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,34 @@ using System;
 
 public class Player : MonoBehaviour {
 
-
     public CharachterStats charachterStats;
 
     public int currentHealth;
     public int maxHealth;
 
     public LevelExperienceHelper PlayerLevel { get; set; }
+    public int currentLevel;
+ //   public int rewardsWaiting;
 
     // Use this for initialization
     void Awake () {
 
         PlayerLevel = GetComponent<LevelExperienceHelper>();
+        currentLevel = 0; // PlayerLevel.Level;
+        //      rewardsWaiting = 0;
 
-        // not setting in the window at first
+        //        charachterStats = new CharachterStats(4, 10, 2);
+        charachterStats = new CharachterStats(4, 5, 2, 5, 4, 2, 3);
+
+        maxHealth = (charachterStats.GetStat(BaseStat.BaseStatType.Health).GetCalculatedStatValue() * 10)
+            + charachterStats.GetStat(BaseStat.BaseStatType.Vitality).GetCalculatedStatValue();
+
         this.currentHealth = this.maxHealth;
 
-//        charachterStats = new CharachterStats(4, 10, 2);
-        charachterStats = new CharachterStats(4, 10, 2, 5, 4, 2, 3);
-
-
-
+        // not setting in the window at first
         UIEventHandler.PlayerHealthChanged(this.currentHealth, this.maxHealth);
 
         Debug.Log("Player is created and awakened");
-
     }
 
     public void TakeDamage(int amount)
@@ -56,7 +59,27 @@ public class Player : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-	
+    void Update()
+    {
+        if (currentLevel != PlayerLevel.Level)
+        {
+            Debug.Log("PLAYER LEVEL UP");
+            LevelUpStats();
+        }
 	}
+
+    void LevelUpStats()// might be better to just add a listener onto expHelper
+    {
+        currentLevel++;
+     //   rewardsWaiting += currentLevel;
+
+        for (int i = 0; i < currentLevel; i++)
+        {
+            int luckyStat = UnityEngine.Random.Range(0, charachterStats.stats.Count);
+            charachterStats.stats[luckyStat].BaseValue ++;
+            UIEventHandler.StatsChanged();
+            Debug.Log("PLAYER RECIEVED BASE STAT INCREACE");
+        }
+
+    }
 }
