41a4167db3000e0eeb3c61259408407a1454cf90
bryan foley <s00156670@mail.itsligo.ie>
2017-08-29 18:42:16 +0100

    setting up treasure chest

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,10 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Chest : ActionItem {
+
+    public List<Item> chestItems;
+    private InventoryController inventory;
+
+    private void Start()
+    {
+        chestItems = new List<Item>();
+        inventory = GameObject.Find("Player").GetComponent<InventoryController>();
+
+        ItemDatabase db = GameObject.Find("Inventory").GetComponent<ItemDatabase>();
+        chestItems.Add(db.GetItem("ball"));
+    }
+
 
     public override void Interact()
     {
         Debug.Log("Interacting with Treasure Chest");
+        foreach (Item item in chestItems)
+        {
+            // add item to inventory
+            Debug.Log("Item from chest is " + item.ItemName);
+            inventory.GiveItem(item);
+        }
+    //    GameObject.Destroy(this);
+    }
+
+    public void StoreItem(Item item)
+    {
+        chestItems.Add(item);
     }
 }
