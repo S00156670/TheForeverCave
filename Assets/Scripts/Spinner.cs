815e8fdc1d0cbb73093da0e479007acb9fe8691e
bryan foley <s00156670@mail.itsligo.ie>
2017-08-30 08:03:45 +0100

    deamon shield refined and set to spin

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * 100);
+    }
+}
